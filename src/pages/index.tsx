71e1d0ef2343bc2c8c1208d57a65e64fce33e1ad
DaviJat <91758001+DaviJat@users.noreply.github.com>
2023-04-17 15:22:33 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,21 +5,8 @@ import { useState } from 'react';
 export default function Home() {
   const [user, setUser] = useState<UserProps | null>(null);
 
-  const loadUser = async (userName: string, userPassword: string) => {
-    console.log(userName, userPassword);
-
-    // const res = await fetch(`https://api.github.com/users/${userName}`);
-
-    // const data = await res.json();
-
-    // const { email, password } = data;
-
-    // const userData: UserProps = {
-    //   email,
-    //   password,
-    // };
-
-    // setUser(userData);
+  const loadUser = async (userNome: string, userEmail: string, userSenha: string) => {
+    console.log(userNome, userEmail, userSenha);
   };
 
   return (
